856c9ff096cc34cd915205a53e97744996024932
Bipin <bipp2009@gmail.com>
2021-08-20 16:51:03 +0100

    Landing page and re direction to login page

---
+++ controllers/home-routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const {Post, Comment, User} = require('../models/index')
+
+//landing page of website -> we want to display all posts in db
+router.get('/', async (req, res) => {
+    res.render('all-posts')
+})
+
+//login endpoint -> login handlebars will be displayed when the user lands on /login
+router.get('/login', async (req, res) =>{
+    res.render('login')
+})
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,2 +1,6 @@
-const router = require('express').Router();
+const router = require("express").Router();
+
+const homeRoutes = require("./home-routes");
+router.use("/", homeRoutes);
+
 module.exports = router;
