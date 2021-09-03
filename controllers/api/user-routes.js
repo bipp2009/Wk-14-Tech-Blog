48bf4edc52bd5723720349e06a797d4c78c4b7ee
Bipin <bipp2009@gmail.com>
2021-09-03 15:36:34 +0100

    login.js page api coded  and working correctly

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,31 +1,59 @@
 const router = require("express").Router();
-const { User }= require('../../models')
-
+const { User } = require("../../models");
 
 //Endpoint for creating a user
-router.post('/', async (req,res) => {
-    console.log("/api/user endpoint triggered")
-    console.log(req.body)
-    try {
-        const userData = await User.create(req.body);
-    
-        req.session.save(() => {
-          req.session.user_id = userData.id;
-          req.session.username = userData.name;
-          req.session.logged_in = true;
-    
-          res.status(200).json(userData);
-        });
-      } catch (err) {
-        res.status(400).json(err);
-      }
-})
-//logging in a user
-router.post('/login', async (req, res) => {
+router.post("/", async (req, res) => {
+  console.log("/api/user endpoint triggered");
+  console.log(req.body);
+  try {
+    const userData = await User.create(req.body);
 
-})
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.username = userData.name;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+//logging in a user
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    
+    if (!user) {
+      console.log("Users Issue")
+      res
+        .status(400)
+        .json({
+          message: "Email address or password incorrect please try again",
+        });
+      return;
+    }
+    const validPassword = await user.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({
+          message: "Email address or password incorrect please try again",
+        });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.username = user.name;
+      req.session.logged_in = true;
+      res.json({ user: user, message: "You are now successfully logged in" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 //Logging out a user
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -35,6 +63,4 @@ router.post('/logout', (req, res) => {
   }
 });
 
-
-
-module.exports = router; 
+module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -6,5 +6,25 @@ const password = document.querySelector("#exampleInputPassword1").value
 
 console.log("email: ", email);
 console.log("Password: ", password)
+
+if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/user/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+
 }
-document.querySelector ("#login").addEventListener("submit", loginUser) 
+document.querySelector("#login").addEventListener("submit", loginUser)
+
+  
+  
